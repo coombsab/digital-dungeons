84c9e73a4fcad97f6f625d8d2d9f6bb4e5bb853c
Thomas Daily <111809425+Thomas-Daily@users.noreply.github.com>
2022-10-12 11:13:27 -0600

    working on sidebar

--- digital-dungeons.client/src/components/Navbar.vue
+++ digital-dungeons.client/src/components/Navbar.vue
@@ -1,19 +1,14 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <!-- <div class="row">
+    <div class="col-2"> -->
+  <div class="d-flex flex-column">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo" src="../assets/img/Digital_Dungeon_Logo.png" height="45" />
       </div>
     </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
@@ -27,7 +22,29 @@
       <!-- LOGIN COMPONENT HERE -->
       <Login />
     </div>
-  </nav>
+  </div>
+  <!-- <nav class="sidebar navbar navbar-expand-lg navbar-dark px-3">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <div class="d-flex flex-column align-items-center">
+        <img alt="logo" src="../assets/img/Digital_Dungeon_Logo.png" height="45" />
+      </div>
+    </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto">
+        <li>
+          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
+            About
+          </router-link>
+        </li>
+      </ul> -->
+  <!-- LOGIN COMPONENT HERE -->
+  <!-- <Login />
+    </div>
+  </nav> -->
 </template>
 
 <script>
@@ -41,6 +58,10 @@ export default {
 </script>
 
 <style scoped>
+.sidebar {
+  background-color: #dc1a22;
+}
+
 a:hover {
   text-decoration: none;
 }
