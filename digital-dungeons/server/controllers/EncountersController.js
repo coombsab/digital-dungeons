626f1e8ad038bfb5a78f8bab9b2ac7a94c8eddc5
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-10-12 16:53:00 -0600

    Hoo HOO

--- digital-dungeons/server/controllers/EncountersController.js
+++ digital-dungeons/server/controllers/EncountersController.js
@@ -10,7 +10,8 @@ export class EncountersController extends BaseController {
       .get("/:encounterId/monsters", this.getMonstersByEncounterId)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post("", this.addEncounter)
-      .delete("/:encounterId", this.removeEncounter);
+      .delete("/:encounterId", this.removeEncounter)
+      .put("/:encounterId", this.editEncounter);
   }
 
   async getMonstersByEncounterId(req, res, next) {
@@ -34,21 +35,22 @@ export class EncountersController extends BaseController {
     }
   }
 
-  async editEncounter(req, res, next) {
-    try {
-      req.body.creatorId = req.userInfo.id;
-      const encounter = await encountersService.addEncounter(req.body);
-      res.send(encounter);
-    } catch (error) {
-      next(error);
-    }
-  }
   async removeEncounter(req, res, next) {
     try {
       const encounter = await encountersService.removeEncounter(
         req.params.encounterId,
         req.userInfo.id
       );
+      res.send(encounter);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async editEncounter(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id;
+      req.body.id = req.params.encounterId;
+      const encounter = await encountersService.editEncounter(req.body);
       res.send(encounter);
     } catch (error) {
       next(error);
