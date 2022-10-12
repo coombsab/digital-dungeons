d2b240a7f9bc85792c325092cec51091c738e382
Thomas Daily <111809425+Thomas-Daily@users.noreply.github.com>
2022-10-12 12:25:52 -0600

    defualted campaign cover img

--- digital-dungeons.client/src/models/Campaign.js
+++ digital-dungeons.client/src/models/Campaign.js
@@ -5,6 +5,6 @@ export class Campaign {
     this.name = data.name
     this.creatorId = data.creatorId
     this.desc = data.desc
-    this.coverImg = data.coverImg
+    this.coverImg = data.coverImg || 'https://i.pinimg.com/originals/db/98/1d/db981db279d5988e238855faa73dce9e.jpg'
   }
 }
