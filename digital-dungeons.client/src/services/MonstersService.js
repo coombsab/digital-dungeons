d868a8ea78e87c3bee01b9ea389b71d5b78ae3ba
Abraham Coombs <coombsab@gmail.com>
2022-10-12 11:42:07 -0600

    FE axiosservice and FE monstersservice

---
+++ digital-dungeons.client/src/services/MonstersService.js
@@ -0,0 +1,20 @@
+class MonstersService {
+  async getApiMonsters(params) {
+
+  }
+  async getApiMonsterByIndex(monsterIndex) {
+
+  }
+
+  async getMonsters(params) {
+
+  }
+  async getMonstersByEncounterId(encounterId) {
+
+  }
+  async addMonster(monsterData) {
+
+  }
+}
+ 
+export const monstersService = new MonstersService()
