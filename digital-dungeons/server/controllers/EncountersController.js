e3e284c7ccbc9b5dab3db71fcf15b8887d73df89
Abraham Coombs <coombsab@gmail.com>
2022-10-12 14:11:09 -0600

    encounterscontroller

--- digital-dungeons/server/controllers/EncountersController.js
+++ digital-dungeons/server/controllers/EncountersController.js
@@ -1,3 +1,40 @@
-export class EncountersController {
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController";
 
+export class EncountersController extends BaseController {
+  constructor() {
+    super("api/encounters")
+    this.router
+      .get("", this.getEncounters)
+      .get("/:encounterId/monsters", this.getMonstersByEncounterId)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post("", this.addEncounter
+  }
+
+  async getEncounters(req, res, next) {
+    try {
+    
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async getMonstersByEncounterId(req, res, next) {
+    try {
+    
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async addMonster(req, res, next) {
+    try {
+    
+    }
+    catch(error) {
+      next(error)
+    }
+  } 
 }
