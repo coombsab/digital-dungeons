b3d44feb69a57e943ddf4a8c85378084964fc735
Talor Montandon <tt.montandon@gmail.com>
2022-10-12 16:57:28 -0600

    All Campaigns and My Campaigns drawn to page

--- digital-dungeons.client/src/components/CampaignCard.vue
+++ digital-dungeons.client/src/components/CampaignCard.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="campaign-card">
+  <div class="campaign-card my-2">
     <div class="card">
-      <div class="card-header"></div>
-      <div class="card-body"></div>
+      <div class="card-header">{{campaign.name}}</div>
+      <div class="card-body">{{campaign.desc}}</div>
       <div class="card-footer"></div>
     </div>
   </div>
 </template>
 
 <script>
+import { Campaign } from '../models/Campaign.js';
+
 export default {
+  props: {
+    campaign: { type: Campaign, required: true }
+  },
   setup() {
     return {};
   },
--- digital-dungeons.client/src/pages/CampaignsPage.vue
+++ digital-dungeons.client/src/pages/CampaignsPage.vue
@@ -1,38 +1,52 @@
 <template>
-  <div class="campaigns-page">
-
-    {{campaigns}}
+  <div class="campaigns-page text-danger">
+    <!-- ADD CAMPAIGN BUTTON -->
+    <button class="btn btn-danger">Create Campaigns</button>
+    <!-- MY CAMPAIGNS -->
+    <div class="border-bottom border-3 border-danger my-3 mb-4">
+      <h3>My Campaigns</h3>
+    </div>
+    <CampaignCard v-for="c in userCampaigns" :key="c.id" :campaign="c" />
+    <!-- ALL CAMPAIGNS -->
+    <div class="border-bottom border-3 border-danger my-3">
+      <h3>All Campaigns</h3>
+    </div>
+    <CampaignCard v-for="c in campaigns" :key="c.id" :campaign="c" />
   </div>
 </template>
 
 <script>
 import { computed } from '@vue/reactivity';
-import { onMounted } from 'vue';
+import { onMounted, watchEffect } from 'vue';
 import { AppState } from '../AppState.js';
 import { campaignsService } from '../services/CampaignsService.js';
 import Pop from '../utils/Pop.js';
+import CampaignCard from '../components/CampaignCard.vue';
 
 
 
 export default {
   setup() {
     onMounted(() => {
+      getCampaigns();
+    });
+    watchEffect(() => {
       getCampaigns()
-    })
-
+    });
     async function getCampaigns() {
       try {
-        await campaignsService.getCampaigns()
-      } catch (error) {
-        Pop.error(error, '[getCampaigns]')
+        await campaignsService.getCampaigns();
+      }
+      catch (error) {
+        Pop.error(error, "[getCampaigns]");
       }
     }
-
     return {
-      campaigns: computed(() => AppState.campaigns)
-
+      campaigns: computed(() => AppState.campaigns),
+      userCampaigns: computed(() => AppState.campaigns.filter(c => c.creatorId == AppState.account.id))
     };
   },
+  components: { CampaignCard }
 };
 </script>
 
