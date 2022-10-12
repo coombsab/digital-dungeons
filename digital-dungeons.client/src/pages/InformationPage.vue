da5bd2ab78b1244037d4518cef3896a39967eb61
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-10-12 11:35:08 -0600

    Pages

---
+++ digital-dungeons.client/src/pages/InformationPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="information-page">
+    <h1>Welcome to the Information Page... You filthy Animal 🐒</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- digital-dungeons.client/src/router.js
+++ digital-dungeons.client/src/router.js
@@ -1,32 +1,47 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import { authGuard } from '@bcwdev/auth0provider-client'
+import { createRouter, createWebHashHistory } from "vue-router";
+import { authGuard } from "@bcwdev/auth0provider-client";
 
 function loadPage(page) {
-  return () => import(`./pages/${page}.vue`)
+  return () => import(`./pages/${page}.vue`);
 }
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: loadPage('HomePage')
+    path: "/",
+    name: "Home",
+    component: loadPage("HomePage"),
   },
   {
-    path: '/about',
-    name: 'About',
-    component: loadPage('AboutPage')
+    path: "/information",
+    name: "Information",
+    component: loadPage("InformationPage"),
   },
   {
-    path: '/account',
-    name: 'Account',
-    component: loadPage('AccountPage'),
-    beforeEnter: authGuard
-  }
-]
+    path: "/campaigns",
+    name: "Campaigns",
+    component: loadPage("CampaignsPage"),
+  },
+  {
+    path: "/campaigns/:campaignId",
+    name: "CampaignDetails",
+    component: loadPage("CampaignDetailsPage"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: loadPage("AboutPage"),
+  },
+  {
+    path: "/account",
+    name: "Account",
+    component: loadPage("AccountPage"),
+    beforeEnter: authGuard,
+  },
+];
 
 export const router = createRouter({
-  linkActiveClass: 'router-link-active',
-  linkExactActiveClass: 'router-link-exact-active',
+  linkActiveClass: "router-link-active",
+  linkExactActiveClass: "router-link-exact-active",
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
