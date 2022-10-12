b02399686c6a8d39fb82b866a3b9771f03786b6d
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-10-12 11:36:00 -0600

    Encounter Card

--- digital-dungeons.client/src/components/EncounterCard.vue
+++ digital-dungeons.client/src/components/EncounterCard.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="encounter-card"></div>
+  <div class="encounter-card">
+    <div class="card">
+      <div class="card-header"></div>
+      <div class="card-body"></div>
+      <div class="card-footer"></div>
+    </div>
+  </div>
 </template>
 
 <script>
