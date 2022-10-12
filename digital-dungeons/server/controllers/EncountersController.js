a3251c6654a0945fae08c05dfaccaad928573286
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-10-12 15:40:35 -0600

    remove encounters

--- digital-dungeons/server/controllers/EncountersController.js
+++ digital-dungeons/server/controllers/EncountersController.js
@@ -5,32 +5,43 @@ import BaseController from "../utils/BaseController";
 
 export class EncountersController extends BaseController {
   constructor() {
-    super("api/encounters")
+    super("api/encounters");
     this.router
       .get("/:encounterId/monsters", this.getMonstersByEncounterId)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post("", this.addEncounter)
+      .delete("/:encounterId", this.removeEncounter);
   }
-
 
   async getMonstersByEncounterId(req, res, next) {
     try {
-      const monsters = await monstersService.getMonstersByEncounterId(req.params.id)
-      res.send(monsters)
-    }
-    catch (error) {
-      next(error)
+      const monsters = await monstersService.getMonstersByEncounterId(
+        req.params.id
+      );
+      res.send(monsters);
+    } catch (error) {
+      next(error);
     }
   }
 
   async addEncounter(req, res, next) {
     try {
-      req.body.creatorId = req.userInfo.id
-      const encounter = await encountersService.addEncounter(req.body)
-      res.send(encounter)
+      req.body.creatorId = req.userInfo.id;
+      const encounter = await encountersService.addEncounter(req.body);
+      res.send(encounter);
+    } catch (error) {
+      next(error);
     }
-    catch (error) {
-      next(error)
+  }
+  async removeEncounter(req, res, next) {
+    try {
+      const encounter = await encountersService.removeEncounter(
+        req.params.id,
+        req.userInfo
+      );
+      res.send(encounter);
+    } catch (error) {
+      next(error);
     }
   }
 }
