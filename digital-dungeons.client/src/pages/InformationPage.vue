4a17150aafdfdfdede9c4b4d32e2f6fc508c53b3
Abraham Coombs <coombsab@gmail.com>
2022-10-13 11:27:26 -0600

    info page changes

--- digital-dungeons.client/src/pages/InformationPage.vue
+++ digital-dungeons.client/src/pages/InformationPage.vue
@@ -2,16 +2,26 @@
   <div class="information-page">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-6">
-          <button class="btn">RULES</button>
+        <div class="col-6 p-3">
+          <div class="text-center">
+            <button class="btn btn-success elevation-2">RULES</button>
+          </div>
         </div>
-        <div class="col-6">
-          <button class="btn">GAME MECHANICS</button>
+        <div class="col-6 p-3">
+          <div class="text-center">
+            <button class="btn btn-success elevation-2">GAME MECHANICS</button>
+          </div>
         </div>
       </div>
       <div class="row">
-        <div class="col-4"></div>
-        <div class="col-8"></div>
+        <div class="col-4 text-center pt-5">
+          <span class="text-danger fs-5"><strong>DROPDOWN</strong></span>
+        </div>
+        <div class="col-8">
+          <div class="info-content">
+
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -25,4 +35,25 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .information-page {
+    background-image: url("https://thumbs.dreamstime.com/b/aged-dirty-paper-texture-stained-background-grunge-design-87575620.jpg");
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+    height: 100vh;
+  }
+  button {
+    font-weight: 800;
+    font-size: 64px;
+    padding-left: 2rem;
+    padding-right: 2rem;
+    border-radius: 2rem;
+  }
+
+  .info-content {
+    background-color: black;
+    height: 100%;
+    width: 100%;
+  }
+</style>
