d9428874671faec466e8b30b374f727a0fba06e2
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-10-13 11:25:30 -0600

    fixed stuff

--- digital-dungeons.client/src/components/CampaignCard.vue
+++ digital-dungeons.client/src/components/CampaignCard.vue
@@ -1,20 +1,22 @@
 <template>
-  <router-link :to="{name: "CampaignDetails", params: {campaignId: campaign.id}}">
-  <div class="campaign-card my-2 selectable">
-    <div class="card">
-      <div class="card-header d-flex justify-content-between">
-        <span>{{ campaign.name }}</span
-        ><i
-          v-if="account.id == campaign.creatorId"
-          class="mdi mdi-delete mx-3 selectable rounded fs-4"
-          @click.stop="removeCampaign(campaign.id)"
-        ></i>
+  <router-link
+    :to="{ name: 'CampaignDetails', params: { campaignId: campaign.id } }"
+  >
+    <div class="campaign-card my-2 selectable">
+      <div class="card">
+        <div class="card-header d-flex justify-content-between">
+          <span>{{ campaign.name }}</span
+          ><i
+            v-if="account.id == campaign.creatorId"
+            class="mdi mdi-delete mx-3 selectable rounded fs-4"
+            @click.stop="removeCampaign(campaign.id)"
+          ></i>
+        </div>
+        <div class="card-body">{{ campaign.desc }}</div>
+        <div class="card-footer"></div>
       </div>
-      <div class="card-body">{{ campaign.desc }}</div>
-      <div class="card-footer"></div>
     </div>
-  </div>
-   </router-link>
+  </router-link>
 </template>
 
 <script>
