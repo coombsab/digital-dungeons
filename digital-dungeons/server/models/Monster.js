bcb2b4d41655fc1666ee3dbfcfb4f0d9e07e19d1
Abraham Coombs <coombsab@gmail.com>
2022-10-13 10:00:09 -0600

    monster schema reorg

--- digital-dungeons/server/models/Monster.js
+++ digital-dungeons/server/models/Monster.js
@@ -2,18 +2,20 @@ import { Schema } from 'mongoose'
 import { OBJECT_ID, SCHEMA_OPTIONS } from "../db/DbUtils"
 
 export const MonsterSchema = new Schema({
-  name: { type: String, required: true },
-  image: { type: String },
   index: { type: String, required: true },
-  desc: { type: String, maxlength: 500 },
   encounterId: { type: OBJECT_ID, required: true },
-  hit_points: { type: Number, required: true },
+  name: { type: String, required: true },
   type: { type: String, required: true },
   size: { type: String, required: true },
   alignment: { type: String },
-  icon: { type: String, required: true },
   initiative: { type: Number, min: 1 },
-  nickName: { type: String, maxlength: 50, minlength: 1 }
+  
+  //EDITABLE FIELDS
+  hit_points: { type: Number, required: true },
+  nickName: { type: String, maxlength: 50, minlength: 1 },
+  desc: { type: String, maxlength: 500 },
+  image: { type: String },
+  icon: { type: String, required: true },
 
 }, SCHEMA_OPTIONS)
 
