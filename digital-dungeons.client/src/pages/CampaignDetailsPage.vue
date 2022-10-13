d6d2bef6923f34e7c128a4c13205543641a82790
Talor Montandon <tt.montandon@gmail.com>
2022-10-13 14:51:58 -0600

    Merge conflict incoming!!!

--- digital-dungeons.client/src/pages/CampaignDetailsPage.vue
+++ digital-dungeons.client/src/pages/CampaignDetailsPage.vue
@@ -1,7 +1,36 @@
 <template>
-  <div class="campaign-details-page">
-    <div class="text-light">{{ campaign }}</div>
+  <div class="p-3">
+    <div v-if="campaign" class="h00 elevated rounded" :style="{backgroundImage: `url(${campaign.coverImg})`}">
+      <div class="text-light h00 glass">
+        <section class="row">
+          <div class="col-7"></div>
+          <div class="col-3"></div>
+          <!-- ADD ENCOUNTER -->
+          <div class="col-2">
+            <button v-if="campaign." class="btn btn-danger" type="button" data-bs-toggle="modal"
+              data-bs-target="#CreateEncounterModal">
+              Create Encounter
+            </button>
+          </div>
+          <!-- LEAVE BUTTON -->
+          <!-- <button></button> -->
+          <!-- JOIN BUTTON -->
+          <div class="col-2">
+            <!-- <button></button> -->
+          </div>
+
+        </section>
+        <section class="row"></section>
+
+
+
+
+      </div>
+    </div>
   </div>
+
+  <!-- MODAL COMPONENT -->
+  <CreateEncounterModal />
 </template>
 
 <script>
@@ -10,13 +39,15 @@ import { useRoute } from "vue-router";
 import { AppState } from "../AppState.js";
 import { campaignsService } from "../services/CampaignsService.js";
 import Pop from "../utils/Pop.js";
+import CreateEncounterModal from "../components/CreateEncounterModal.vue";
 export default {
   setup() {
     const route = useRoute();
     async function getCampaignById() {
       try {
         await campaignsService.getCampaignById(route.params.campaignId);
-      } catch (error) {
+      }
+      catch (error) {
         Pop.error(error);
       }
     }
@@ -25,9 +56,21 @@ export default {
     });
     return {
       campaign: computed(() => AppState.activeCampaign),
+      account: computed(() => AppState.account)
     };
   },
+  components: { CreateEncounterModal }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.h00 {
+  height: 96vh;
+  background-size: cover;
+  background-position: center;
+}
+
+.glass {
+  background-color: rgba(38, 37, 37, 0.397);
+}
+</style>
