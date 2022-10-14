97272818117de3999d7bafc6ea9cd5b9bfe94ef3
Thomas Daily <111809425+Thomas-Daily@users.noreply.github.com>
2022-10-14 10:48:38 -0600

    change api

--- digital-dungeons.client/src/services/AxiosService.js
+++ digital-dungeons.client/src/services/AxiosService.js
@@ -9,3 +9,8 @@ export const dndApi = Axios.create({
   baseURL: "https://www.dnd5eapi.co",
   timeout: 4000
 })
+
+export const openDndApi = Axios.create({
+  baseURL: "https://api-beta.open5e.com",
+  timeout: 4000
+})
