f6623be905f9582551de5003e177aa2513ca7c65
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-10-14 11:31:33 -0600

    Deleting and getting encounters

--- digital-dungeons.client/src/pages/CampaignDetailsPage.vue
+++ digital-dungeons.client/src/pages/CampaignDetailsPage.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="p-3">
-    <div v-if="campaign" class="h00 elevated rounded" :style="{backgroundImage: `url(${campaign.coverImg})`}">
+    <div
+      v-if="campaign"
+      class="h00 elevated rounded"
+      :style="{ backgroundImage: `url(${campaign.coverImg})` }"
+    >
       <div class="text-light h00 glass">
         <section class="row justify-content-between">
           <div class="col-7 bg-dark p-2 text-center">
-            <div class=" bg-secondary text-white">
-              <h2> {{campaign.name}}</h2>
+            <div class="bg-secondary text-white">
+              <h2>{{ campaign.name }}</h2>
             </div>
           </div>
           <!-- NOTE Cant input Dm's Name because creator of campaign is not populated on campaign -->
@@ -16,8 +20,13 @@
           </div> -->
           <!-- ADD ENCOUNTER -->
           <div class="col-2">
-            <button v-if="campaign.creatorId == account.id" class="btn btn-danger" type="button" data-bs-toggle="modal"
-              data-bs-target="#CreateEncounterModal">
+            <button
+              v-if="campaign.creatorId == account.id"
+              class="btn btn-danger"
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#CreateEncounterModal"
+            >
               Create Encounter
             </button>
           </div>
@@ -33,28 +42,41 @@
         </section>
         <section class="row">
           <div class="col-6">
-            <img :src="campaign.coverImg" alt="" class="img-fluid">
+            <img :src="campaign.coverImg" alt="" class="img-fluid" />
             <div class="bg-dark p-2">
               <div class="bg-secondary p-1">
-                <p>{{campaign.desc}}</p>
+                <p>{{ campaign.desc }}</p>
               </div>
             </div>
           </div>
           <div class="col-6">
             <div class="bg-dark p-2">
               <div class="bg-secondary p-1">
-                <h4 class="border-bottom border-3 border-danger text-danger">Completed Encounters</h4>
-                <EncounterCard :encounter="completedEncounters" />
-                <h4 class="border-bottom border-3 border-danger text-danger">Encounters</h4>
-                <EncounterCard :encounter="uncompletedEncounters" />
+                <h4
+                  class="border-bottom border-3 border-danger text-danger"
+                  v-if="completedEncounters.length > 0"
+                >
+                  Completed Encounters
+                </h4>
+                <EncounterCard
+                  v-for="e in completedEncounters"
+                  :encounter="e"
+                  :key="e?.id"
+                  :campaign="campaign"
+                />
+                <h4 class="border-bottom border-3 border-danger text-danger">
+                  Encounters
+                </h4>
+                <EncounterCard
+                  v-for="e in uncompletedEncounters"
+                  :encounter="e"
+                  :key="e?.id"
+                  :campaign="campaign"
+                />
               </div>
             </div>
           </div>
         </section>
-
-
-
-
       </div>
     </div>
   </div>
@@ -71,28 +93,43 @@ import { campaignsService } from "../services/CampaignsService.js";
 import Pop from "../utils/Pop.js";
 import CreateEncounterModal from "../components/CreateEncounterModal.vue";
 import EncounterCard from "../components/EncounterCard.vue";
+import { encountersService } from "../services/EncountersService.js";
 export default {
   setup() {
     const route = useRoute();
     async function getCampaignById() {
       try {
         await campaignsService.getCampaignById(route.params.campaignId);
+      } catch (error) {
+        Pop.error(error);
       }
-      catch (error) {
+    }
+    async function getEncountersByCampaignId() {
+      try {
+        await encountersService.getEncountersByCampaignId(
+          route.params.campaignId
+        );
+        console.log("Getting Encounters");
+      } catch (error) {
         Pop.error(error);
       }
     }
     onMounted(() => {
       getCampaignById();
+      getEncountersByCampaignId();
     });
     return {
       campaign: computed(() => AppState.activeCampaign),
       account: computed(() => AppState.account),
-      completedEncounters: computed(() => AppState.encounters.filter(e => e.isCompleted)),
-      uncompletedEncounters: computed(() => AppState.encounters.filter(e => !e.isCompleted))
+      completedEncounters: computed(() =>
+        AppState.encounters.filter((encounters) => encounters.isCompleted)
+      ),
+      uncompletedEncounters: computed(() =>
+        AppState.encounters.filter((encounters) => !encounters.isCompleted)
+      ),
     };
   },
-  components: { CreateEncounterModal, EncounterCard }
+  components: { CreateEncounterModal, EncounterCard },
 };
 </script>
 
