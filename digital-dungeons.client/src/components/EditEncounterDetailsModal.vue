05dac1d0bf9a3f82d69456166c8b39e83c9be630
Talor Montandon <tt.montandon@gmail.com>
2022-10-14 14:05:35 -0600

    Modal pop up on encounter card click

---
+++ digital-dungeons.client/src/components/EditEncounterDetailsModal.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="component">
+    <div class="modal fade" id="encounterModal" tabindex="-1" aria-labelledby="encounterModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content bg-secondary">
+          <div class="modal-header">
+            <h5 class="modal-title" id="encounterModalLabel">New Encounter</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form action="submit" class="card text-secondary" @submit.prevent="addEncounter()">
+              <div class="card-body text-start">
+                <div class="form-floating mb-3">
+                  <input type="text" class="form-control" v-model="editable.name" required name="name"
+                    placeholder="Name:" maxlength="50" />
+                  <label for="name">Name:</label>
+                </div>
+                <div class="form-floating mb-3">
+                  <input type="url" class="form-control" v-model="editable.coverImg" name="coverImg"
+                    placeholder="Cover Image:" maxlength="10000" />
+                  <label for="coverImg">Cover Image:</label>
+                </div>
+                <div class="form-floating">
+                  <textarea type="text" class="form-control textarea-height" v-model="editable.desc" name="description"
+                    style="resize: none" placeholder="Description:" maxlength="500"></textarea>
+                  <label for="description">Description:</label>
+                </div>
+                <div class="text-end">
+                  <span>{{editable.desc ? editable.desc.length : 0}}</span>
+                  <span>/ 500</span>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                  Close
+                </button>
+                <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">
+                  Create An Ecounter!
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    let editable = ref({})
+    return {
+      editable
+
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- digital-dungeons.client/src/components/EncounterCard.vue
+++ digital-dungeons.client/src/components/EncounterCard.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="encounter-card text-white my-2" v-if="encounter">
-    <div
-      class="card"
-      :style="{ backgroundImage: `url(${encounter?.coverImg})` }"
-    >
+    <div class="card" :style="{ backgroundImage: `url(${encounter?.coverImg})` }">
       <div class="glass rounded">
-        <i
-          v-if="account.id == campaign.creatorId"
+        <i v-if="account.id == campaign.creatorId"
           class="mdi mdi-delete selectable rounded fs-6 delete-icon text-visible text-end"
-          @click.stop="removeEncounter(encounter.id)"
-        ></i>
-        <!-- Modal link -->
-        <div class="card-header">
-          <h5>{{ encounter?.name }}</h5>
+          @click.stop="removeEncounter(encounter.id)"></i>
+
+        <div class="selectable" type="button" data-bs-toggle="modal" data-bs-target="#encounterModal">
+          <div class="card-header">
+            <h5>{{ encounter?.name }}</h5>
+          </div>
+          <div class="card-body">
+            <p>{{ encounter?.desc }}</p>
+            <p>{{ encounter?.type }}</p>
+          </div>
+          <div class="card-footer"></div>
         </div>
-        <div class="card-body">
-          <p>{{ encounter?.desc }}</p>
-          <p>{{ encounter?.type }}</p>
-        </div>
-        <div class="card-footer"></div>
       </div>
     </div>
   </div>
+
+  <EditEncounterDetailsModal v-if="account.id == campaign.creatorId" />
+  <EncounterDetailsModal v-else />
 </template>
 
 <script>
@@ -29,6 +29,7 @@ import { computed } from "vue";
 import { AppState } from "../AppState.js";
 import { encountersService } from "../services/EncountersService.js";
 import Pop from "../utils/Pop.js";
+import EncounterDetailsModal from "./EncounterDetailsModal.vue";
 
 export default {
   props: {
@@ -40,26 +41,27 @@ export default {
       account: computed(() => AppState.account),
       async removeEncounter(id) {
         try {
-          const yes = await Pop.confirm(
-            "Are you sure you want to delete this Campaign?"
-          );
+          const yes = await Pop.confirm("Are you sure you want to delete this Campaign?");
           if (!yes) {
             return;
           }
           await encountersService.removeEncounter(id);
-        } catch (error) {
+        }
+        catch (error) {
           Pop.error(error);
         }
       },
     };
   },
+  components: { EncounterDetailsModal }
 };
 </script>
 
 <style lang="scss" scoped>
 .glass {
-  background-color: rgba(38, 37, 37, 0.469);
+  background-color: rgba(85, 3, 3, 0.739);
 }
+
 .card {
   background-size: cover;
   background-position: center;
