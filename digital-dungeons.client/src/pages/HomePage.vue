d94befaf39d70173063bf23f4975279c7facfb64
Abraham Coombs <coombsab@gmail.com>
2022-10-14 10:30:01 -0600

    home page styling

--- digital-dungeons.client/src/pages/HomePage.vue
+++ digital-dungeons.client/src/pages/HomePage.vue
@@ -1,25 +1,23 @@
 <template>
-  <section class="container-fluid">
-    <div class="row">
-      <div class="text-center col-12 my-5">
-        <h1 class="DD-title text-shadow"><span class="DD-border">WELCOME TO DIGITAL DUNGEONS</span></h1>
-      </div>
+  <section class="home d-flex flex-column">
+    <div class="title-container text-center">
+      <h1 class="DD-title text-shadow"><span class="DD-border">WELCOME TO DIGITAL DUNGEONS</span></h1>
     </div>
-    <div class="row justify-content-around align-items-center mt-5">
-      <div class="col-5 text-center">
-        <router-link :to="{ name: 'Campaigns' }">
-          <img class="home-img col-5 border border-danger border-4 rounded elevation-4"
-            src="https://preview.redd.it/nme1k9p45eo21.jpg?auto=webp&s=e7beecea363fa0a547ce09b0cca2872a2019447e"
-            alt="Campaign" title="Campaigns">
-        </router-link>
-      </div>
-      <div class="col-5 text-center">
-        <router-link :to="{ name: 'Information' }">
-          <img class="home-img col-5 border border-success border-4 rounded elevation-4"
-            src="https://i.pinimg.com/originals/da/72/e6/da72e6588befda673cc0fcdf64a49e40.jpg" alt="Info"
-            title="Information">
-        </router-link>
-      </div>
+    <div class="cards-container justify-content-around align-items-center d-flex flex-wrap flex-grow-1">
+      <router-link :to="{ name: 'Campaigns' }">
+        <div class="theme-card campaigns elevation-3">
+          <div class="on-md-hover text-visible">
+            <span class="popup-text">Campaigns</span>
+          </div>
+        </div>
+      </router-link>
+      <router-link :to="{ name: 'Information' }">
+        <div class="theme-card information elevation-3">
+          <div class="on-md-hover text-visible">
+            <span class="popup-text">Information</span>
+          </div>
+        </div>
+      </router-link>
     </div>
   </section>
 </template>
@@ -27,21 +25,58 @@
 <script>
 export default {
   setup() {
-    return {}
+    return {
+
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
 .home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-
+  height: 100vh;
+  position: relative;
 }
+
+.title-container {
+  margin-top: 5rem;
+  margin-bottom: 3rem;
+}
+
+.popup-text {
+  font-size: 48px;
+}
+
+.theme-card {
+  height: 30rem;
+  width: 22rem;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: 150ms linear;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+.theme-card:hover {
+  transform: scale(1.1);
+}
+
+.campaigns {
+  background-image: url("https://preview.redd.it/nme1k9p45eo21.jpg?auto=webp&s=e7beecea363fa0a547ce09b0cca2872a2019447e");
+  background-position: center;
+  background-size: cover;
+}
+.information {
+  background-image: url("https://i.pinimg.com/originals/da/72/e6/da72e6588befda673cc0fcdf64a49e40.jpg");
+  background-position: center;
+  background-size: cover;
+}
+
+
 
 .DD-title {
   font-family: "Morpheus";
@@ -55,18 +90,33 @@ export default {
   letter-spacing: 0.08rem
 }
 
-
-.home-img {
-  max-height: 60vh;
-  width: 29vw;
-  width: 100%;
-  object-fit: cover;
-  object-position: center;
-}
-
 .DD-border {
   border-top: 6px solid;
   border-bottom: 6px solid;
   border-color: rgba(130, 129, 25, 0.749);
+}
+
+@media (min-width: 768px) {
+  .theme-card {
+    background-size: cover;
+    background-position: center;
+    background-color: black;
+    height: 32rem;
+    width: 24rem;
+    border-radius: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .on-md-hover {
+    transition: all 0.2s linear;
+    opacity: 0;
+    pointer-events: none;
+  }
+  *:hover > .on-md-hover {
+    opacity: 1;
+    pointer-events: all;
+  }
 }
 </style>
