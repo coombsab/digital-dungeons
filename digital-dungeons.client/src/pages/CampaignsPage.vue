31b21166b55aadbf39ba760b470335dd90e64ca8
Jake Paulin <jpaulin@zagmail.gonzaga.edu>
2022-10-17 09:48:31 -0600

    someting I dont know

--- digital-dungeons.client/src/pages/CampaignsPage.vue
+++ digital-dungeons.client/src/pages/CampaignsPage.vue
@@ -1,12 +1,20 @@
 <template>
   <section class="campaigns-wrapper">
-    <div class="campaigns-page text-visible py-4 ">
+    <div class="campaigns-page text-visible py-4">
       <!-- ADD CAMPAIGN BUTTON -->
-      <button class="btn text-visible create-button elevation-2" type="button" data-bs-toggle="modal" data-bs-target="#campaignModal">
+      <button
+        class="btn text-visible create-button elevation-2"
+        type="button"
+        data-bs-toggle="modal"
+        data-bs-target="#campaignModal"
+      >
         Create Campaign
       </button>
       <!-- MY CAMPAIGNS -->
-      <div class="border-bottom border-3 border-color my-3 mb-4 ps-2" v-if="userCampaigns.length > 0">
+      <div
+        class="border-bottom border-3 border-color my-3 mb-4 ps-2"
+        v-if="userCampaigns.length > 0"
+      >
         <h3>My Campaigns</h3>
       </div>
       <div class="d-flex flex-wrap justify-content-around">
@@ -21,38 +29,80 @@
       </div>
     </div>
     <!-- MODAL close this if its driving you crazy -->
-    <div class="modal fade" id="campaignModal" tabindex="-1" aria-labelledby="campaignModalLabel" aria-hidden="true">
+    <div
+      class="modal fade"
+      id="campaignModal"
+      tabindex="-1"
+      aria-labelledby="campaignModalLabel"
+      aria-hidden="true"
+    >
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="campaignModalLabel">New Campaign</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
           </div>
           <div class="modal-body">
             <form action="submit" class="card" @submit.prevent="addCampaign()">
               <div class="card-body text-start">
                 <div class="form-floating mb-3">
-                  <input type="text" class="form-control" v-model="editable.name" required name="name" placeholder="Name:" maxlength="50"/>
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="editable.name"
+                    required
+                    name="name"
+                    placeholder="Name:"
+                    maxlength="50"
+                  />
                   <label for="name">Name:</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input type="url" class="form-control" v-model="editable.coverImg" required name="coverImg" placeholder="coverImg" maxlength="10000"/>
+                  <input
+                    type="url"
+                    class="form-control"
+                    v-model="editable.coverImg"
+                    required
+                    name="coverImg"
+                    placeholder="coverImg"
+                    maxlength="10000"
+                  />
                   <label for="coverImg">Cover Image:</label>
                 </div>
                 <div class="form-floating">
-                  <textarea class="form-control textarea-height" v-model="editable.desc" name="description" style="resize: none" placeholder="Descripotion:" maxlength="500"></textarea>
+                  <textarea
+                    class="form-control textarea-height"
+                    v-model="editable.desc"
+                    name="description"
+                    style="resize: none"
+                    placeholder="Descripotion:"
+                    maxlength="500"
+                  ></textarea>
                   <label for="description">Description:</label>
                 </div>
                 <div class="text-end">
-                  <span>{{editable.desc ? editable.desc.length : 0}}</span>
+                  <span>{{ editable.desc ? editable.desc.length : 0 }}</span>
                   <span>/ 500</span>
                 </div>
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
                   Close
                 </button>
-                <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                  data-bs-dismiss="modal"
+                >
                   Start Your Adventure!
                 </button>
               </div>
@@ -117,31 +167,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .campaigns-wrapper {
-    height: 100vh;
-    overflow-y: auto;
-  }
-  .border-color {
-    border-color: rgb(96, 190, 234) !important;
-  }
+.campaigns-wrapper {
+  height: 100vh;
+  overflow-y: auto;
+}
+.border-color {
+  border-color: rgb(96, 190, 234) !important;
+}
 
-  .textarea-height {
-    height: 10.8rem;
-  }
+.textarea-height {
+  height: 10.8rem;
+}
 
-  .campaigns-page {
-    position: relative;
-  }
+.campaigns-page {
+  position: relative;
+}
 
-  .create-button {
-    position: absolute;
-    right: 1.5rem;
-    top: 2rem;
-    // border-color: rgb(96, 190, 234);
-    background-color: rgba(0, 0, 0, 0.639);
-  }
+.create-button {
+  position: absolute;
+  right: 1.5rem;
+  top: 2rem;
+  // border-color: rgb(96, 190, 234);
+  background-color: rgba(0, 0, 0, 0.639);
+}
 
-  .create-button:hover {
-    background-color: rgba(255, 255, 255, 0.142);
-  }
+.create-button:hover {
+  background-color: rgba(255, 255, 255, 0.142);
+}
 </style>
