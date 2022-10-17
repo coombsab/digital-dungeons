edaae5cf85b75cc5ef99b9fa5dd664fc2d4ed36c
Talor Montandon <tt.montandon@gmail.com>
2022-10-17 16:26:35 -0600

    prt2

--- digital-dungeons.client/src/components/InformationCards/ArmorCard.vue
+++ digital-dungeons.client/src/components/InformationCards/ArmorCard.vue
@@ -1,56 +1,57 @@
 <template>
-  <div class="spell-card p-3 rounded elevation-2 text-light selectable" title="See Spell Details" data-bs-toggle="modal"
-    data-bs-target="#spellModal" @click="setActiveSpell()">
-    <span>{{spell.name}}</span>
+  <div class="monster-card p-3 rounded elevation-2 text-light selectable" title="See Monster Details"
+    data-bs-toggle="modal" :data-bs-target="'#monsterModal' + monster.slug" @click="">
+    <span>{{monster.name}}</span>
   </div>
 
-  <div class="modal fade" id="spellModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" :id="'monsterModal' + monster.slug" tabindex="-1" aria-labelledby="MonsterDetailsModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">{{activeSpell?.name}}</h1>
+          <h1 class="modal-title fs-5" id="MonsterDetailsModalLabel">{{monster?.name}}</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body" v-if="activeSpell">
+        <div class="modal-body" v-if="monster">
           <div class="d-flex flex-wrap justify-content-around mb-3">
             <div>
               <h6 class="statistics">Hit Dice</h6>
-              <p>{{activeSpell.hit_dice}}</p>
+              <p>{{monster.hit_dice}}</p>
             </div>
             <div>
               <h6 class="statistics">Size</h6>
-              <p>{{activeSpell.size}}</p>
+              <p>{{monster.size}}</p>
             </div>
             <div>
               <h6 class="statistics">Alignment</h6>
-              <p>{{activeSpell.alignment}}</p>
+              <p>{{monster.alignment}}</p>
             </div>
             <div>
               <h6 class="statistics">Health</h6>
-              <p>{{activeSpell.hit_points}}</p>
+              <p>{{monster.hit_points}}</p>
             </div>
           </div>
           <div class="d-flex flex-wrap justify-content-around mb-3">
             <div>
               <h6 class="statistics">Type</h6>
-              <p>{{activeSpell.type}}</p>
+              <p>{{monster.type}}</p>
             </div>
             <div>
               <h6 class="statistics">Size</h6>
-              <p>{{activeSpell.size}}</p>
+              <p>{{monster.size}}</p>
             </div>
             <div>
               <h6 class="statistics">Alignment</h6>
-              <p>{{activeSpell.alignment}}</p>
+              <p>{{monster.alignment}}</p>
             </div>
             <div>
               <h6 class="statistics">Health</h6>
-              <p>{{activeSpell.hit_points}}</p>
+              <p>{{monster.hit_points}}</p>
             </div>
           </div>
         </div>
         <div class="modal-body" v-else>
-          <p>Sorry, there is no spell data available :(</p>
+          <p>Sorry, there is no monster data available :(</p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -64,37 +65,24 @@
 
 
 <script>
-import { computed } from '@vue/reactivity';
-import { AppState } from '../AppState';
-// import { spellsService } from '../services/SpellsService';
-import { logger } from '../utils/Logger';
-import Pop from '../utils/Pop';
+import { Monster } from '../../models/Monster.js';
+
 
 
 export default {
   props: {
-    sp: { type: Object, required: true }
+    monster: { type: Monster, required: true }
   },
   setup(props) {
     return {
-      activeSpell: computed(() => AppState.activeSpell),
 
-      setActiveSpell() {
-        try {
-          const spellSlug = props.spell.slug
-          // spellsService.setActiveSpell(spellSlug)
-        } catch (error) {
-          Pop.error(error)
-          logger.log(error)
-        }
-      }
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.spell-card {
+.monster-card {
   background-color: green;
   height: fit-content;
   margin-bottom: 1.5rem;
