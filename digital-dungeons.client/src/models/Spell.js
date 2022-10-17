bacdeb40fca83dc7eb277a115ad1d01cda4ad897
Abraham Coombs <coombsab@gmail.com>
2022-10-17 12:09:09 -0600

    spell model started

---
+++ digital-dungeons.client/src/models/Spell.js
@@ -0,0 +1,27 @@
+export class Spell {
+  constructor(data) {
+
+    
+    // "slug": "abhorrent-apparition",
+    // "name": "Abhorrent Apparition",
+    // "desc": "You imbue a terrifying visage onto a gourd and toss it ahead of you to a spot of your choosing within range. Each creature within 15 feet of that spot takes 6d8 psychic damage and becomes frightened of you for 1 minute; a successful Wisdom saving throw halves the damage and negates the fright. A creature frightened in this way repeats the saving throw at the end of each of its turns, ending the effect on itself on a success.\n",
+    // "higher_level": "If you cast this spell using a spell slot of 5th level or higher, the damage increases by 1d8 for each slot level above 4th.",
+    // "page": "",
+    // "range": "60 feet",
+    // "components": "M",
+    // "material": "a gourd with a face carved on it",
+    // "ritual": "no",
+    // "duration": "Instantaneous",
+    // "concentration": "no",
+    // "casting_time": "1 action",
+    // "level": "4th-level",
+    // "level_int": 4,
+    // "school": "illusion",
+    // "dnd_class": "Bard, Sorcerer, Wizard",
+    // "archetype": "",
+    // "circles": "",
+    // "document__slug": "dmag",
+    // "document__title": "Deep Magic for 5th Edition",
+    // "document__license_url": "http://open5e.com/legal"
+  }
+}
