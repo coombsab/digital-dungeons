5326b214a8f373def9310df1af94183491f2d00f
Talor Montandon <tt.montandon@gmail.com>
2022-10-19 09:48:03 -0600

    Armor model

--- digital-dungeons.client/src/models/Weapon.js
+++ digital-dungeons.client/src/models/Weapon.js
@@ -1,5 +1,12 @@
 export class Weapon {
   constructor(data) {
-    
+    this.name = data.name
+    this.slug = data.slug
+    this.category = data.category
+    this.cost = data.cost
+    this.damage_dice = data.damage_dice
+    this.damage_type = data.damage_type
+    this.weight = data.weight
+    this.properties = data.properties
   }
 }
