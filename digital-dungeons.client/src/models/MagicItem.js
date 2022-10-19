ba04591abc42514f63f0c9947a1398d666b01407
Abraham Coombs <coombsab@gmail.com>
2022-10-19 09:47:56 -0600

    armor and magicitem

--- digital-dungeons.client/src/models/MagicItem.js
+++ digital-dungeons.client/src/models/MagicItem.js
@@ -1,5 +1,12 @@
 export class MagicItem {
   constructor(data) {
-    
+    this.name = data.name
+    this.slug = data.slug
+    this.desc = data.desc
+    this.type = data.type
+    this.rarity = data.rarity
+    this.requires_attunement = data.requires_attunement
+    this.document__slug = data.document__slug
+    this.document__title = data.document__title
   }
 }
