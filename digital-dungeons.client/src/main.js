cbb6c794ccb408b5e35dec4657540ffb31d4eee3
Talor Montandon <tt.montandon@gmail.com>
2022-10-20 11:59:25 -0600

    ClassCard Changes

--- digital-dungeons.client/src/main.js
+++ digital-dungeons.client/src/main.js
@@ -3,6 +3,7 @@ import 'bootstrap'
 import { createApp } from 'vue'
 // @ts-ignore
 import App from './App.vue'
+import Markdown from 'vue3-markdown-it'
 import { registerGlobalComponents } from './registerGlobalComponents'
 import { router } from './router'
 
@@ -11,4 +12,5 @@ registerGlobalComponents(root)
 
 root
   .use(router)
+  .use(Markdown)
   .mount('#app')
