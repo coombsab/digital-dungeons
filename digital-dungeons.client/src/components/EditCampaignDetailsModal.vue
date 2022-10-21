4d199852c25e44d09d9e7a51a6a8e52c13ae8074
Talor Montandon <tt.montandon@gmail.com>
2022-10-21 15:27:48 -0600

    prt 2

--- digital-dungeons.client/src/components/EditCampaignDetailsModal.vue
+++ digital-dungeons.client/src/components/EditCampaignDetailsModal.vue
@@ -1,66 +1,33 @@
 <template>
   <div class="component">
-    <div
-      class="modal fade"
-      :id="'campaignModalEdit' + campaign?.id"
-      tabindex="-1"
-      aria-labelledby="campaignModalEdit"
-      aria-hidden="true"
-    >
+    <div class="modal fade" :id="'campaignModalEdit' + campaign?.id" tabindex="-1" aria-labelledby="campaignModalEdit"
+      aria-hidden="true">
       <div class="modal-dialog modal-lg">
-        <div class="modal-content bg-secondary">
+        <div class="modal-content bg-secondary text-light">
           <div class="modal-header">
             <h5 class="modal-title" id="campaignModalEdit">
               Edit {{ campaign?.name }}
             </h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-              @click="resetForm()"
-            ></button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+              @click="resetForm()"></button>
           </div>
           <div class="modal-body">
-            <form
-              action="submit"
-              class="card text-secondary"
-              @submit.prevent="editCampaign(campaign.id)"
-            >
+            <form action="submit" class="bg-secondary " @submit.prevent="editCampaign(campaign.id)">
               <div class="card-body text-start">
                 <div class="form-floating mb-3">
-                  <input
-                    type="text"
-                    class="form-control"
-                    v-model="editable.name"
-                    required
-                    name="name"
-                    placeholder="Name:"
-                    maxlength="50"
-                  />
+                  <input type="text" class="form-control bg-danger text-white" v-model="editable.name" required
+                    name="name" placeholder="Name:" maxlength="50" />
                   <label for="name">Name:</label>
                 </div>
                 <div class="form-floating mb-3">
-                  <input
-                    type="url"
-                    class="form-control"
-                    v-model="editable.coverImg"
-                    name="coverImg"
-                    placeholder="Cover Image:"
-                    maxlength="10000"
-                  />
+                  <input type="url" class="form-control bg-danger text-white" v-model="editable.coverImg"
+                    name="coverImg" placeholder="Cover Image:" maxlength="10000" />
                   <label for="coverImg">Cover Image:</label>
                 </div>
                 <div class="form-floating">
-                  <textarea
-                    type="text"
-                    class="form-control textarea-height"
-                    v-model="editable.desc"
-                    name="description"
-                    style="resize: none"
-                    placeholder="Description:"
-                    maxlength="1000"
-                  ></textarea>
+                  <textarea type="text" class="form-control textarea-height bg-danger text-white"
+                    v-model="editable.desc" name="description" style="resize: none" placeholder="Description:"
+                    maxlength="1000"></textarea>
                   <label for="description">Description:</label>
                 </div>
                 <div class="text-end">
@@ -69,19 +36,10 @@
                 </div>
               </div>
               <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                  @click="resetForm()"
-                >
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="resetForm()">
                   Close
                 </button>
-                <button
-                  type="submit"
-                  class="btn btn-primary"
-                  data-bs-dismiss="modal"
-                >
+                <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">
                   Edit this Campaign!
                 </button>
               </div>
