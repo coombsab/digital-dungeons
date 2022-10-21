f562611c437380a83e50f0c8620a9efc6b370389
Abraham Coombs <coombsab@gmail.com>
2022-10-21 14:13:33 -0600

    code cleanup & styling fix for active monster card

--- digital-dungeons.client/src/components/ActiveEncounterMonsters.vue
+++ digital-dungeons.client/src/components/ActiveEncounterMonsters.vue
@@ -457,46 +457,6 @@ export default {
 
 
 <style lang="scss" scoped>
-.monster-card {
-  background-color: black;
-  background-color: rgba(0, 0, 0, 0.6);
-  height: 10rem;
-  width: 8rem;
-  border-radius: 0.5rem;
-  background-position: center;
-  background-size: cover;
-  transition: 150ms linear;
-}
-
-.monster-card:hover {
-  transform: scale(1.1);
-}
-
-.input-bg {
-  background-color: rgba(0, 0, 0, 0.552) !important;
-}
-
-.modal-content {
-  background-position: center;
-  background-size: cover;
-}
-
-.bg-transparent {
-  background-color: rgba(10, 10, 10, 0.804) !important;
-}
-
-.bg-transparent:hover {
-  background-color: rgba(10, 10, 10, 0.43) !important;
-}
-
-.bg-transparent-modal {
-  background-color: rgba(10, 10, 10, 0.664) !important;
-}
-
-.all-transparent {
-  background-color: rgba(0, 0, 0, 0) !important;
-}
-
 .muted-layer {
   height: 10rem;
   width: 8rem;
@@ -524,7 +484,74 @@ export default {
   align-items: center;
 }
 
-.textarea-height {
+.scrollable {
+  overflow-y: auto;
+  max-height: 70vh;
+}
+
+.text-woah {
+  color: rgb(96, 190, 234) !important;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.644), 0px 0px 5px rgba(18, 34, 50, 0.678);
+}
+
+.monster-card {
+  background-color: black;
+  background-color: rgba(0, 0, 0, 0.6);
   height: 10rem;
+  width: 8rem;
+  border-radius: 0.5rem;
+  background-position: center;
+  background-size: cover;
+  transition: 150ms linear;
+}
+
+.monster-card:hover {
+  transform: scale(1.1);
+}
+
+.modal-content {
+  background-position: center;
+  background-size: cover;
+}
+
+.bg-transparent {
+  background-color: rgba(10, 10, 10, 0.804) !important;
+}
+
+.bg-transparent:hover {
+  background-color: rgba(10, 10, 10, 0.303) !important;
+}
+
+.bg-transparent-modal {
+  background-color: rgba(10, 10, 10, 0.442) !important;
+}
+
+.muted-layer {
+  height: 10rem;
+  width: 8rem;
+  border-radius: 0.5rem;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  padding: 0.5rem;
+}
+
+.statistics {
+  border-bottom: 1px solid rgb(172, 170, 170);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.text-visible {
+  color: rgb(216, 224, 229) !important;
+}
+
+.text-visible:hover {
+  color: rgb(216, 224, 229) !important;
+}
+
+.text-light:hover {
+  color: rgb(216, 224, 229) !important;
 }
 </style>
