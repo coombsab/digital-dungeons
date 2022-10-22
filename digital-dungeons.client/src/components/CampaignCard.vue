dbd205d38cc83264eb48216ec8b0d4ec016061d0
Abraham Coombs <coombsab@gmail.com>
2022-10-21 20:46:48 -0600

    campaigncard text adjustment on hover

--- digital-dungeons.client/src/components/CampaignCard.vue
+++ digital-dungeons.client/src/components/CampaignCard.vue
@@ -1,26 +1,15 @@
 <template>
-  <div
-    class="rounded-3 theme-card my-4 elevation-3 animate__animated animate__fadeIn"
-    :style="{ backgroundImage: `url(${campaign.coverImg})` }"
-  >
+  <div class="rounded-3 theme-card my-4 elevation-3 animate__animated animate__fadeIn"
+    :style="{ backgroundImage: `url(${campaign.coverImg})` }">
     <div class="muted-layer rounded-3 text-end">
       <div class="dropdown on-hover">
-        <button
-          class="btn back dropdown-toggle text-visible px-2"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-          v-if="account.id == campaign.creatorId"
-        >
+        <button class="btn back dropdown-toggle text-visible px-2" type="button" data-bs-toggle="dropdown"
+          aria-expanded="false" v-if="account.id == campaign.creatorId">
           Options
         </button>
         <ul class="dropdown-menu">
           <li class="dropdown-item">
-            <button
-              class="btn"
-              data-bs-toggle="modal"
-              :data-bs-target="'#campaignModalEdit' + campaign.id"
-            >
+            <button class="btn" data-bs-toggle="modal" :data-bs-target="'#campaignModalEdit' + campaign.id">
               Edit Campaign
             </button>
           </li>
@@ -32,18 +21,12 @@
         </ul>
       </div>
 
-      <router-link
-        :to="{ name: 'CampaignDetails', params: { campaignId: campaign.id } }"
-      >
+      <router-link :to="{ name: 'CampaignDetails', params: { campaignId: campaign.id } }">
         <div
-          class="card-content d-flex flex-column flex-wrap justify-content-around text-center color-shift animate__animated animate__fadeIn"
-        >
+          class="card-content d-flex flex-column flex-wrap justify-content-around text-center color-shift animate__animated animate__fadeIn">
           <span class="pb-5 fs-4 text-visible">{{ campaign.name }}</span>
-          <span
-            class="flex-grow-1 mt-5 desc fs-6"
-            :class="campaign.desc.length > 120 ? 'text-truncate' : ''"
-            >{{ campaign.desc }}</span
-          >
+          <span class="flex-grow-1 mt-5 desc fs-6" :class="campaign.desc.length > 120 ? 'text-truncate' : ''">{{
+          campaign.desc }}</span>
           <!-- <span class="" v-if="campaign.desc.length > 120"><em>Truncated desc as it was too long (> 120
               chars)</em></span> -->
         </div>
@@ -137,8 +120,6 @@ export default {
 
 .color-shift:hover {
   color: rgb(96, 190, 234);
-  text-shadow: 1px 1px rgba(0, 0, 0, 0.644),
-    0px 0px 5px rgba(189, 234, 30, 0.678);
 }
 
 .theme-card:hover {
