f7e254903d9217b3cedce9261df99c586f2c83b9
Lumine3449 <>
2022-10-23 13:15:08 -0600

    about page carousel update

--- digital-dungeons.client/src/pages/AboutPage.vue
+++ digital-dungeons.client/src/pages/AboutPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="container-fluid about-page">
+  <!-- <section class="container-fluid about-page">
     <div class="row justify-content-around ">
 
 
@@ -95,8 +95,94 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
+
+
+
+
+  <!-- SECTION: dev team images and desc -->
+  <h1 class="mt-5">Our Amazing Team:</h1>
+
+  <div class="container">
+    <div class="carousel">
+      <input type="radio" id="carousel-1" name="carousel[]" checked>
+      <input type="radio" id="carousel-2" name="carousel[]">
+      <input type="radio" id="carousel-3" name="carousel[]">
+      <input type="radio" id="carousel-4" name="carousel[]">
+      <input type="radio" id="carousel-5" name="carousel[]">
+      <input type="radio" id="carousel-6" name="carousel[]">
+      <input type="radio" id="carousel-7" name="carousel[]">
+
+
+      <ul class="carousel__items">
+        <!-- Ky Want -->
+        <li class="carousel__item"><img
+            src="https://images.unsplash.com/photo-1561389248-231c46a75a97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGN1dGUlMjBjYXR8ZW58MHx8MHx8&w=1000&q=80"
+            alt="Kylee Want Picture" class="carousel_img img-fluid">
+        </li>
+
+        <!-- Abraham Coombs -->
+        <li class="carousel__item"><img src="../assets/img/abraham.jpg" alt="Abraham Coombs Picture"
+            class="carousel_img img-fluid"></li>
+
+        <!-- Jake Neitzell -->
+        <li class="carousel__item"><img src="../assets/img/me.jpg" alt="Jake Neitzell Picture"
+            class="carousel_img img-fluid">
+        </li>
+
+        <!-- Talor Montandon -->
+        <li class="carousel__item"><img
+            src="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg"
+            alt="Talor Montandon Picture" class="carousel_img img-fluid"></li>
+
+        <!-- Thomas Daily -->
+        <li class="carousel__item"><img
+            src="https://c1.wallpaperflare.com/preview/108/852/910/cat-mackerel-funny-red.jpg"
+            alt="Thomas Daily Picture" class="carousel_img img-fluid">
+        </li>
+
+        <!-- Jake Paulin -->
+        <li class="carousel__item"><img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm8ErlMlPdnS5zbhJ2KL339H-cCsjrxjMl8A&usqp=CAU"
+            alt="Jake Paulin Picture" class="carousel_img img-fluid">
+        </li>
+      </ul>
+
+
+      <div class="carousel__prev">
+        <label for="carousel-1"></label>
+        <label for="carousel-2"></label>
+        <label for="carousel-3"></label>
+        <label for="carousel-4"></label>
+        <label for="carousel-5"></label>
+        <label for="carousel-6"></label>
+      </div>
+
+      <div class="carousel__next">
+        <label for="carousel-1"></label>
+        <label for="carousel-2"></label>
+        <label for="carousel-3"></label>
+        <label for="carousel-4"></label>
+        <label for="carousel-5"></label>
+        <label for="carousel-6"></label>
+      </div>
+
+      <div class="carousel__nav">
+        <label for="carousel-1"></label>
+        <label for="carousel-2"></label>
+        <label for="carousel-3"></label>
+        <label for="carousel-4"></label>
+        <label for="carousel-5"></label>
+        <label for="carousel-6"></label>
+      </div>
+    </div>
+  </div>
+
+
 </template>
+
+
+
 
 
 
@@ -105,7 +191,6 @@
 export default {
   setup() {
     return {
-
     }
   }
 }
@@ -114,7 +199,248 @@ export default {
 
 
 
+
+
+
+
 <style lang="scss" scoped>
+// #region: carousel
+.carousel_img {}
+
+%animation-default {
+  opacity: 1 !important;
+  z-index: 3;
+}
+
+@mixin carousel($items, $animation: 'default') {
+  .carousel {
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+
+    >input[type="radio"] {
+      position: absolute;
+      left: 0;
+      opacity: 0;
+      top: 0;
+
+      &:checked {
+
+        ~.carousel__items .carousel__item,
+        ~.carousel__prev>label,
+        ~.carousel__next>label {
+          opacity: 0;
+        }
+      }
+
+      @for $i from 1 through $items {
+        &:nth-child(#{$i}) {
+          &:checked {
+            ~.carousel__items .carousel__item {
+              @if $animation =='default' {
+                &:nth-child(#{$i}) {
+                  opacity: 1;
+                }
+              }
+            }
+
+            ~.carousel__prev {
+              >label {
+                @if $i ==1 {
+                  &:nth-child(#{$items}) {
+                    @extend %animation-default;
+                  }
+                }
+
+                @else if $i ==$items {
+                  &:nth-child(#{$items - 1}) {
+                    @extend %animation-default;
+                  }
+                }
+
+                @else {
+                  &:nth-child(#{$i - 1}) {
+                    @extend %animation-default;
+                  }
+                }
+              }
+            }
+
+            ~.carousel__next {
+              >label {
+                @if $i ==$items {
+                  &:nth-child(1) {
+                    @extend %animation-default;
+                  }
+                }
+
+                @else {
+                  &:nth-child(#{$i + 1}) {
+                    @extend %animation-default;
+                  }
+                }
+              }
+            }
+
+            ~.carousel__nav {
+              >label {
+                &:nth-child(#{$i}) {
+                  background: #ccc;
+                  cursor: default;
+                  pointer-events: none;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    &__items {
+      margin: 0;
+      padding: 0;
+      list-style-type: none;
+      width: 100%;
+      height: 600px;
+      position: relative;
+    }
+
+    &__item {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 1;
+      transition: opacity 2s;
+      -webkit-transition: opacity 2s;
+
+      img {
+        width: 100%;
+        vertical-align: middle;
+      }
+    }
+
+    &__prev,
+    &__next {
+      >label {
+        border: 1px solid #fff;
+        border-radius: 50%;
+        cursor: pointer;
+        display: block;
+        width: 40px;
+        height: 40px;
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        -webkit-transform: translateY(-50%);
+        transition: all .3s ease;
+        -webkit-transition: all .3s ease;
+        opacity: 0;
+        z-index: 2;
+
+        &:hover,
+        &:focus {
+          opacity: .5 !important;
+        }
+
+        &:before,
+        &:after {
+          content: "";
+          position: absolute;
+          width: inherit;
+          height: inherit;
+        }
+
+        &:before {
+          background: linear-gradient(to top, #fff 0%, #fff 10%, rgba(51, 51, 51, 0) 10%),
+            linear-gradient(to left, #fff 0%, #fff 10%, rgba(51, 51, 51, 0) 10%);
+          width: 60%;
+          height: 60%;
+          top: 20%;
+        }
+      }
+    }
+
+    &__prev {
+      >label {
+        left: 2%;
+
+        &:before {
+          left: 35%;
+          top: 20%;
+          transform: rotate(135deg);
+          -webkit-transform: rotate(135deg);
+        }
+      }
+    }
+
+    &__next {
+      >label {
+        right: 2%;
+
+        &:before {
+          left: 10%;
+          transform: rotate(315deg);
+          -webkit-transform: rotate(315deg);
+        }
+      }
+    }
+
+    &__nav {
+      position: absolute;
+      bottom: 3%;
+      left: 0;
+      text-align: center;
+      width: 100%;
+      z-index: 3;
+
+      >label {
+        border: 1px solid #fff;
+        display: inline-block;
+        border-radius: 50%;
+        cursor: pointer;
+        margin: 0 .125%;
+        width: 20px;
+        height: 20px;
+      }
+    }
+  }
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+}
+
+body {
+  background: #fcfcfc;
+  margin: 0;
+}
+
+.container {
+  width: 900px;
+  min-width: 900px;
+  margin: 50px auto;
+}
+
+@include carousel(6);
+// #endregion
+
+
+
+h1 {
+  color: #fff;
+  font-family: sans-serif;
+  text-align: center;
+  font-weight: normal;
+}
+
+
+// #region: everything else
 .about-page {
   height: 100vh;
   overflow-y: auto;
@@ -158,4 +484,6 @@ a {
     padding: 1rem 5rem 1rem 5rem;
   }
 }
+
+// #endregion
 </style>
