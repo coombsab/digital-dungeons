e59b7d4da4f402404c7cfaa28094e178b1e7dff8
Abraham Coombs <coombsab@gmail.com>
2022-11-03 14:23:46 -0600

    some formatting on about page

--- digital-dungeons.client/src/pages/AboutPage.vue
+++ digital-dungeons.client/src/pages/AboutPage.vue
@@ -22,18 +22,24 @@
               src="https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg"
               alt="Talor Montandon">
             <div class="desc">
-              <div class="icons">
-                <a href="https://github.com/Tmontandon" target="_blank" class="mdi mdi-github fs-4 p-1"
-                  title="GitHub"></a>
-                <a class="mdi mdi-linkedin fs-4 p-1" title="LinkedIn" target="_blank"
-                  href="https://www.linkedin.com/in/talor-montandon-92a792254/"></a>
+              <div class="desc-title d-flex justify-content-center">
+                <h3>Talor Montandon</h3>
+                <div class="icons d-flex gap-2 ps-3">
+                  <a href="https://github.com/Tmontandon" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/talor-montandon-92a792254/"></a>
+                </div>
               </div>
-              <span>Welcome to Digital-Dungeons! Poke around and see our website's features. If you have experience with
-                DnD you may find this website as a useful tool, and if you are new perhaps this website will help to
-                peak your interest. I had a ton of fun learning how build this project and working with a team. Don't
-                forget to
-                check out the other members of our team to see their thoughts and experiences about this project too!
-              </span>
+              <div class="desc-body p-3">
+                <span>Welcome to Digital-Dungeons! Poke around and see our website's features. If you have experience
+                  with
+                  DnD you may find this website as a useful tool, and if you are new perhaps this website will help to
+                  peak your interest. I had a ton of fun learning how build this project and working with a team. Don't
+                  forget to
+                  check out the other members of our team to see their thoughts and experiences about this project
+                  too!</span>
+              </div>
             </div>
           </label>
 
@@ -42,16 +48,20 @@
           <label class="card" for="item-2" id="member-2">
             <img src="../assets/img/abraham.jpg" alt="Abraham Coombs">
             <div class="desc">
-              <div class="icons">
-                <a href="https://github.com/coombsab" target="_blank" class="mdi mdi-github fs-4 p-1"
-                  title="GitHub"></a>
-                <a class="mdi mdi-linkedin fs-4 p-1" title="LinkedIn" target="_blank"
-                  href="https://www.linkedin.com/in/abraham-coombs-33408ab8/"></a>
+              <div class="desc-title d-flex justify-content-center">
+                <h3>Abraham Coombs</h3>
+                <div class="icons d-flex gap-2 ps-3">
+                  <a href="https://github.com/coombsab" target="_blank" class="mdi mdi-github fs-4" title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/abraham-coombs-33408ab8/"></a>
+                </div>
               </div>
-              <span>Fuzzy Wuzzy was a bear. Fuzzy Wuzzy had no hair. Fuzzy Wuzzy wasn't very fuzzy, was he?</span>
-              <hr>
-              <span>How much wood would a woodchuck chuck if a woodchuck could chuck wood? He would chuck what wood a
-                woodchuck could chuck if a woodchuck could chuck wood!</span>
+              <div class="desc-body p-3">
+                <span>Fuzzy Wuzzy was a bear. Fuzzy Wuzzy had no hair. Fuzzy Wuzzy wasn't very fuzzy, was he?</span>
+                <hr>
+                <span>How much wood would a woodchuck chuck if a woodchuck could chuck wood? He would chuck what wood a
+                  woodchuck could chuck if a woodchuck could chuck wood!</span>
+              </div>
             </div>
           </label>
 
@@ -60,16 +70,22 @@
           <label class="card" for="item-3" id="member-3">
             <img src="../assets/img/jake-neitzell.jpg" alt="Jake Neitzell">
             <div class="desc">
-              <div class="icons">
-                <a href="https://github.com/JacobNeitzell" target="_blank" class="mdi mdi-github fs-4 p-1"
-                  title="GitHub"></a>
-                <a class="mdi mdi-linkedin fs-4 p-1" title="LinkedIn" target="_blank"
-                  href="https://www.linkedin.com/in/jacob-neitzell-610a64180/"></a>
+              <div class="desc-title d-flex justify-content-center">
+                <h3>Jake Neitzell</h3>
+                <div class="icons d-flex gap-2 ps-3">
+                  <a href="https://github.com/JacobNeitzell" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/jacob-neitzell-610a64180/"></a>
+                </div>
               </div>
-              <span>Hello I am a junior software developer and this is the project I worked on at school in 2 weeks, I
-                am really satisfied with the way the site came together and hope to continue to make it look better and
-                add more functionality. I hope that you will enjoy the digital dungeons site and be able to use it for
-                your campaigns" there is my description</span>
+              <div class="desc-body p-3">
+                <span>Hello I am a junior software developer and this is the project I worked on at school in 2 weeks, I
+                  am really satisfied with the way the site came together and hope to continue to make it look better
+                  and
+                  add more functionality. I hope that you will enjoy the digital dungeons site and be able to use it for
+                  your campaigns" there is my description</span>
+              </div>
             </div>
           </label>
 
@@ -80,13 +96,18 @@
               src="https://static.scientificamerican.com/sciam/cache/file/92E141F8-36E4-4331-BB2EE42AC8674DD3_source.jpg"
               alt="Jake Paulin">
             <div class="desc">
-              <div class="icons">
-                <a href="https://github.com/Jakeepaulin" target="_blank" class="mdi mdi-github fs-4 p-1"
-                  title="GitHub"></a>
-                <a class="mdi mdi-linkedin fs-4 p-1" title="LinkedIn" target="_blank"
-                  href="https://github.com/Jakeepaulin"></a>
+              <div class="desc-title d-flex justify-content-center">
+                <h3>Jake Paulin</h3>
+                <div class="icons d-flex gap-2 ps-3">
+                  <a href="https://github.com/Jakeepaulin" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://github.com/Jakeepaulin"></a>
+                </div>
               </div>
-              <span>Jake's description</span>
+              <div class="desc-body p-3">
+                <span>Jake's description</span>
+              </div>
             </div>
           </label>
 
@@ -95,14 +116,21 @@
           <label class="card" for="item-5" id="member-5">
             <img src="../assets/img/ThomasDaily.jpg" alt="Thomas Daily">
             <div class="desc">
-              <div class="icons">
-                <a href="https://github.com/Thomas-Daily" target="_blank" class="mdi mdi-github fs-4 p-1"
-                  title="GitHub"></a>
-                <a class="mdi mdi-linkedin fs-4 p-1" title="LinkedIn" target="_blank" href=""></a>
+              <div class="desc-title d-flex justify-content-center">
+                <h3>Thomas Daily</h3>
+                <div class="icons d-flex gap-2 ps-3">
+                  <a href="https://github.com/Thomas-Daily" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://github.com/Thomas-Daily"></a>
+                </div>
               </div>
-              <span>Hi! My name's Tom and I am a junior developer going through CodeWorks. I'm excited to get into the
-                software development workforce and I am excited to see what it is going to hold for me in the future. I
-                know I look like a big guy, but I swear I'm a teddy bear.</span>
+              <div class="desc-body p-3">
+                <span>Hi! My name's Tom and I am a junior developer going through CodeWorks. I'm excited to get into the
+                  software development workforce and I am excited to see what it is going to hold for me in the future.
+                  I
+                  know I look like a big guy, but I swear I'm a teddy bear.</span>
+              </div>
             </div>
           </label>
 
@@ -113,28 +141,34 @@
               src="https://images.unsplash.com/photo-1561389248-231c46a75a97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGN1dGUlMjBjYXR8ZW58MHx8MHx8&w=1000&q=80"
               alt="Ky Want">
             <div class="desc">
-              <div class="icons">
-                <a href="https://github.com/Lumine3449" target="_blank" class="mdi mdi-github fs-4 p-1"
-                  title="GitHub"></a>
-                <a class="mdi mdi-linkedin fs-4 p-1" title="LinkedIn" target="_blank"
-                  href="https://www.linkedin.com/in/kylee-want-559853249/"></a>
+              <div class="desc-title d-flex justify-content-center">
+                <h3>Ky Want</h3>
+                <div class="icons d-flex gap-2 ps-3">
+                  <a href="https://github.com/Lumine3449" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/kylee-want-559853249/"></a>
+                </div>
               </div>
-              <span>Ky's description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsa, quas
-                dolorum cum amet commodi quasi, quidem tempora veniam sunt autem corrupti nisi explicabo odio unde
-                ratione est necessitatibus vitae maxime aliquid error architecto adipisci! Perferendis ut explicabo,
-                obcaecati similique delectus nostrum praesentium. Ipsam excepturi deleniti quo. Fuga laborum magnam,
-                libero repudiandae nesciunt impedit similique et non quod cupiditate quaerat amet fugit dolore molestiae
-                labore at nobis odio nihil animi. Vitae praesentium odit impedit nostrum fugiat corrupti fuga nihil
-                dolorem explicabo? Earum saepe doloremque similique commodi sint ea cum mollitia, aliquid eveniet vel
-                expedita, veniam nemo recusandae ab sunt explicabo corporis neque sequi? Aliquam nulla, exercitationem
-                deleniti optio voluptatibus porro mollitia fugiat neque labore, doloribus voluptas blanditiis ducimus
-                obcaecati aliquid temporibus? Esse labore harum saepe recusandae nam tempore soluta quasi laboriosam
-                nostrum, magni, placeat ullam minus quidem corporis, excepturi sapiente ducimus culpa nisi ipsam
-                ratione? Culpa ea, cupiditate explicabo repellat molestiae non perferendis dolorum soluta hic tempore!
-                Quo quae nemo dolorem aut accusantium quisquam exercitationem. Ipsum eaque perferendis molestiae
-                possimus vel nulla officiis amet veritatis et vero ad quo quia repellat, dolore ratione necessitatibus
-                placeat doloremque ducimus deserunt! Quisquam fuga laborum temporibus eligendi suscipit, praesentium
-                perferendis facilis fugiat nobis voluptate.</span>
+              <div class="desc-body p-3">
+                <span>Ky's description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsa, quas
+                  dolorum cum amet commodi quasi, quidem tempora veniam sunt autem corrupti nisi explicabo odio unde
+                  ratione est necessitatibus vitae maxime aliquid error architecto adipisci! Perferendis ut explicabo,
+                  obcaecati similique delectus nostrum praesentium. Ipsam excepturi deleniti quo. Fuga laborum magnam,
+                  libero repudiandae nesciunt impedit similique et non quod cupiditate quaerat amet fugit dolore
+                  molestiae
+                  labore at nobis odio nihil animi. Vitae praesentium odit impedit nostrum fugiat corrupti fuga nihil
+                  dolorem explicabo? Earum saepe doloremque similique commodi sint ea cum mollitia, aliquid eveniet vel
+                  expedita, veniam nemo recusandae ab sunt explicabo corporis neque sequi? Aliquam nulla, exercitationem
+                  deleniti optio voluptatibus porro mollitia fugiat neque labore, doloribus voluptas blanditiis ducimus
+                  obcaecati aliquid temporibus? Esse labore harum saepe recusandae nam tempore soluta quasi laboriosam
+                  nostrum, magni, placeat ullam minus quidem corporis, excepturi sapiente ducimus culpa nisi ipsam
+                  ratione? Culpa ea, cupiditate explicabo repellat molestiae non perferendis dolorum soluta hic tempore!
+                  Quo quae nemo dolorem aut accusantium quisquam exercitationem. Ipsum eaque perferendis molestiae
+                  possimus vel nulla officiis amet veritatis et vero ad quo quia repellat, dolore ratione necessitatibus
+                  placeat doloremque ducimus deserunt! Quisquam fuga laborum temporibus eligendi suscipit, praesentium
+                  perferendis facilis fugiat nobis voluptate.</span>
+              </div>
             </div>
           </label>
         </div>
@@ -159,6 +193,18 @@ export default {
 
 
 <style lang="scss" scoped>
+.desc-title {
+  position: relative;
+  border-bottom: 2px solid rgba(95, 19, 95, 0.68);
+}
+
+.icons {
+  position: absolute;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+
 .main-section {
   height: 100vh;
 }
