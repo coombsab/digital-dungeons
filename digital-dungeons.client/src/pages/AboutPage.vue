c334bfa1f8332df81dcd03069e542d0acb0d99a6
Lumine3449 <>
2022-11-16 20:20:57 -0700

    ky about update

--- digital-dungeons.client/src/pages/AboutPage.vue
+++ digital-dungeons.client/src/pages/AboutPage.vue
@@ -20,30 +20,20 @@
               <div class="desc-title d-flex justify-content-center">
                 <h3>Talor Montandon</h3>
                 <div class="icons d-flex gap-2 ps-3">
-                  <a
-                    href="https://github.com/Tmontandon"
-                    target="_blank"
-                    class="mdi mdi-github fs-4"
-                    title="GitHub"
-                  ></a>
-                  <a
-                    class="mdi mdi-linkedin fs-4"
-                    title="LinkedIn"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/tmontandon"
-                  ></a>
+                  <a href="https://github.com/Tmontandon" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/tmontandon"></a>
                 </div>
               </div>
               <div class="desc-body p-3">
-                <span
-                  >Welcome to Digital-Dungeons! Poke around and see our
+                <span>Welcome to Digital-Dungeons! Poke around and see our
                   website's features. If you have experience with DnD you may
                   find this website as a useful tool, and if you are new perhaps
                   this website will help to peak your interest. I had a ton of
                   fun learning how build this project and working with a team.
                   Don't forget to check out the other members of our team to see
-                  their thoughts and experiences about this project too!</span
-                >
+                  their thoughts and experiences about this project too!</span>
               </div>
             </div>
           </label>
@@ -55,31 +45,18 @@
               <div class="desc-title d-flex justify-content-center">
                 <h3>Abraham Coombs</h3>
                 <div class="icons d-flex gap-2 ps-3">
-                  <a
-                    href="https://github.com/coombsab"
-                    target="_blank"
-                    class="mdi mdi-github fs-4"
-                    title="GitHub"
-                  ></a>
-                  <a
-                    class="mdi mdi-linkedin fs-4"
-                    title="LinkedIn"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/abraham-coombs-33408ab8/"
-                  ></a>
+                  <a href="https://github.com/coombsab" target="_blank" class="mdi mdi-github fs-4" title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/abraham-coombs-33408ab8/"></a>
                 </div>
               </div>
               <div class="desc-body p-3">
-                <span
-                  >Fuzzy Wuzzy was a bear. Fuzzy Wuzzy had no hair. Fuzzy Wuzzy
-                  wasn't very fuzzy, was he?</span
-                >
+                <span>Fuzzy Wuzzy was a bear. Fuzzy Wuzzy had no hair. Fuzzy Wuzzy
+                  wasn't very fuzzy, was he?</span>
                 <hr />
-                <span
-                  >How much wood would a woodchuck chuck if a woodchuck could
+                <span>How much wood would a woodchuck chuck if a woodchuck could
                   chuck wood? He would chuck what wood a woodchuck could chuck
-                  if a woodchuck could chuck wood!</span
-                >
+                  if a woodchuck could chuck wood!</span>
               </div>
             </div>
           </label>
@@ -91,29 +68,19 @@
               <div class="desc-title d-flex justify-content-center">
                 <h3>Jake Neitzell</h3>
                 <div class="icons d-flex gap-2 ps-3">
-                  <a
-                    href="https://github.com/JacobNeitzell"
-                    target="_blank"
-                    class="mdi mdi-github fs-4"
-                    title="GitHub"
-                  ></a>
-                  <a
-                    class="mdi mdi-linkedin fs-4"
-                    title="LinkedIn"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/jacob-neitzell-610a64180/"
-                  ></a>
+                  <a href="https://github.com/JacobNeitzell" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/jacob-neitzell-610a64180/"></a>
                 </div>
               </div>
               <div class="desc-body p-3">
-                <span
-                  >Hello I am a junior software developer and this is the
+                <span>Hello I am a junior software developer and this is the
                   project I worked on at school in 2 weeks, I am really
                   satisfied with the way the site came together and hope to
                   continue to make it look better and add more functionality. I
                   hope that you will enjoy the digital dungeons site and be able
-                  to use it for your campaigns"</span
-                >
+                  to use it for your campaigns"</span>
               </div>
             </div>
           </label>
@@ -125,23 +92,14 @@
               <div class="desc-title d-flex justify-content-center">
                 <h3>Jake Paulin</h3>
                 <div class="icons d-flex gap-2 ps-3">
-                  <a
-                    href="https://github.com/Jakeepaulin"
-                    target="_blank"
-                    class="mdi mdi-github fs-4"
-                    title="GitHub"
-                  ></a>
-                  <a
-                    class="mdi mdi-linkedin fs-4"
-                    title="LinkedIn"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/jake-paulin/"
-                  ></a>
+                  <a href="https://github.com/Jakeepaulin" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/jake-paulin/"></a>
                 </div>
               </div>
               <div class="desc-body p-3">
-                <span
-                  >Welcome to the wild wild west... stay classy and kind
+                <span>Welcome to the wild wild west... stay classy and kind
                 </span>
               </div>
             </div>
@@ -154,86 +112,42 @@
               <div class="desc-title d-flex justify-content-center">
                 <h3>Thomas Daily</h3>
                 <div class="icons d-flex gap-2 ps-3">
-                  <a
-                    href="https://github.com/Thomas-Daily"
-                    target="_blank"
-                    class="mdi mdi-github fs-4"
-                    title="GitHub"
-                  ></a>
-                  <a
-                    class="mdi mdi-linkedin fs-4"
-                    title="LinkedIn"
-                    target="_blank"
-                    href="https://github.com/Thomas-Daily"
-                  ></a>
+                  <a href="https://github.com/Thomas-Daily" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://github.com/Thomas-Daily"></a>
                 </div>
               </div>
               <div class="desc-body p-3">
-                <span
-                  >Hi! My name's Tom and I am a junior developer going through
+                <span>Hi! My name's Tom and I am a junior developer going through
                   CodeWorks. I'm excited to get into the software development
                   workforce and I am excited to see what it is going to hold for
                   me in the future. I know I look like a big guy, but I swear
-                  I'm a teddy bear.</span
-                >
+                  I'm a teddy bear.</span>
               </div>
             </div>
           </label>
 
           <!-- Ky Want -->
           <label class="card" for="item-6" id="member-6">
-            <img
-              src="https://images.unsplash.com/photo-1561389248-231c46a75a97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGN1dGUlMjBjYXR8ZW58MHx8MHx8&w=1000&q=80"
-              alt="Ky Want"
-            />
+            <img src="../assets/img/ky.jpg" alt="Ky Want" />
             <div class="desc">
               <div class="desc-title d-flex justify-content-center">
                 <h3>Ky Want</h3>
                 <div class="icons d-flex gap-2 ps-3">
-                  <a
-                    href="https://github.com/Lumine3449"
-                    target="_blank"
-                    class="mdi mdi-github fs-4"
-                    title="GitHub"
-                  ></a>
-                  <a
-                    class="mdi mdi-linkedin fs-4"
-                    title="LinkedIn"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/kylee-want-559853249/"
-                  ></a>
+                  <a href="https://github.com/Lumine3449" target="_blank" class="mdi mdi-github fs-4"
+                    title="GitHub"></a>
+                  <a class="mdi mdi-linkedin fs-4" title="LinkedIn" target="_blank"
+                    href="https://www.linkedin.com/in/kylee-want-559853249/"></a>
                 </div>
               </div>
               <div class="desc-body p-3">
-                <span
-                  >Ky's description Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit. Possimus ipsa, quas dolorum cum amet commodi
-                  quasi, quidem tempora veniam sunt autem corrupti nisi
-                  explicabo odio unde ratione est necessitatibus vitae maxime
-                  aliquid error architecto adipisci! Perferendis ut explicabo,
-                  obcaecati similique delectus nostrum praesentium. Ipsam
-                  excepturi deleniti quo. Fuga laborum magnam, libero
-                  repudiandae nesciunt impedit similique et non quod cupiditate
-                  quaerat amet fugit dolore molestiae labore at nobis odio nihil
-                  animi. Vitae praesentium odit impedit nostrum fugiat corrupti
-                  fuga nihil dolorem explicabo? Earum saepe doloremque similique
-                  commodi sint ea cum mollitia, aliquid eveniet vel expedita,
-                  veniam nemo recusandae ab sunt explicabo corporis neque sequi?
-                  Aliquam nulla, exercitationem deleniti optio voluptatibus
-                  porro mollitia fugiat neque labore, doloribus voluptas
-                  blanditiis ducimus obcaecati aliquid temporibus? Esse labore
-                  harum saepe recusandae nam tempore soluta quasi laboriosam
-                  nostrum, magni, placeat ullam minus quidem corporis, excepturi
-                  sapiente ducimus culpa nisi ipsam ratione? Culpa ea,
-                  cupiditate explicabo repellat molestiae non perferendis
-                  dolorum soluta hic tempore! Quo quae nemo dolorem aut
-                  accusantium quisquam exercitationem. Ipsum eaque perferendis
-                  molestiae possimus vel nulla officiis amet veritatis et vero
-                  ad quo quia repellat, dolore ratione necessitatibus placeat
-                  doloremque ducimus deserunt! Quisquam fuga laborum temporibus
-                  eligendi suscipit, praesentium perferendis facilis fugiat
-                  nobis voluptate.</span
-                >
+                <span>Hello! My name is Ky and I'm a junior Full Stack Software Developer graduate from CodeWorks. When
+                  I'm not solving problems in the position of a software developer, I am pouring my creative passion
+                  into drawing and
+                  writing for a lengthy Dungeons and Dragons campaign. I love letting my creativity go wild in
+                  everything I do. Leaving a
+                  creative imprint on anything I touch is important to me.</span>
               </div>
             </div>
           </label>
@@ -346,24 +260,24 @@ img {
   }
 }
 
-#item-1:checked ~ .cards #member-3,
-#item-1:checked ~ .cards #member-4,
-#item-1:checked ~ .cards #member-5,
-#item-2:checked ~ .cards #member-4,
-#item-2:checked ~ .cards #member-5,
-#item-2:checked ~ .cards #member-6,
-#item-3:checked ~ .cards #member-1,
-#item-3:checked ~ .cards #member-5,
-#item-3:checked ~ .cards #member-6,
-#item-4:checked ~ .cards #member-1,
-#item-4:checked ~ .cards #member-2,
-#item-4:checked ~ .cards #member-6,
-#item-5:checked ~ .cards #member-1,
-#item-5:checked ~ .cards #member-2,
-#item-5:checked ~ .cards #member-3,
-#item-6:checked ~ .cards #member-2,
-#item-6:checked ~ .cards #member-3,
-#item-6:checked ~ .cards #member-4 {
+#item-1:checked~.cards #member-3,
+#item-1:checked~.cards #member-4,
+#item-1:checked~.cards #member-5,
+#item-2:checked~.cards #member-4,
+#item-2:checked~.cards #member-5,
+#item-2:checked~.cards #member-6,
+#item-3:checked~.cards #member-1,
+#item-3:checked~.cards #member-5,
+#item-3:checked~.cards #member-6,
+#item-4:checked~.cards #member-1,
+#item-4:checked~.cards #member-2,
+#item-4:checked~.cards #member-6,
+#item-5:checked~.cards #member-1,
+#item-5:checked~.cards #member-2,
+#item-5:checked~.cards #member-3,
+#item-6:checked~.cards #member-2,
+#item-6:checked~.cards #member-3,
+#item-6:checked~.cards #member-4 {
   display: none;
 }
 
@@ -392,34 +306,34 @@ img {
 //   z-index: 0;
 // }
 
-#item-1:checked ~ .cards #member-6,
-#item-2:checked ~ .cards #member-1,
-#item-3:checked ~ .cards #member-2,
-#item-4:checked ~ .cards #member-3,
-#item-5:checked ~ .cards #member-4,
-#item-6:checked ~ .cards #member-5 {
+#item-1:checked~.cards #member-6,
+#item-2:checked~.cards #member-1,
+#item-3:checked~.cards #member-2,
+#item-4:checked~.cards #member-3,
+#item-5:checked~.cards #member-4,
+#item-6:checked~.cards #member-5 {
   transform: translatex(-40%) scale(0.8);
   opacity: 0.4;
   z-index: 0;
 }
 
-#item-1:checked ~ .cards #member-2,
-#item-2:checked ~ .cards #member-3,
-#item-3:checked ~ .cards #member-4,
-#item-4:checked ~ .cards #member-5,
-#item-5:checked ~ .cards #member-6,
-#item-6:checked ~ .cards #member-1 {
+#item-1:checked~.cards #member-2,
+#item-2:checked~.cards #member-3,
+#item-3:checked~.cards #member-4,
+#item-4:checked~.cards #member-5,
+#item-5:checked~.cards #member-6,
+#item-6:checked~.cards #member-1 {
   transform: translatex(40%) scale(0.8);
   opacity: 0.4;
   z-index: 0;
 }
 
-#item-1:checked ~ .cards #member-1,
-#item-2:checked ~ .cards #member-2,
-#item-3:checked ~ .cards #member-3,
-#item-4:checked ~ .cards #member-4,
-#item-5:checked ~ .cards #member-5,
-#item-6:checked ~ .cards #member-6 {
+#item-1:checked~.cards #member-1,
+#item-2:checked~.cards #member-2,
+#item-3:checked~.cards #member-3,
+#item-4:checked~.cards #member-4,
+#item-5:checked~.cards #member-5,
+#item-6:checked~.cards #member-6 {
   transform: translatex(0) scale(1);
   width: 80%;
   opacity: 1;
@@ -431,12 +345,12 @@ img {
   }
 }
 
-#item-1:checked ~ .cards #member-1 > .desc,
-#item-2:checked ~ .cards #member-2 > .desc,
-#item-3:checked ~ .cards #member-3 > .desc,
-#item-4:checked ~ .cards #member-4 > .desc,
-#item-5:checked ~ .cards #member-5 > .desc,
-#item-6:checked ~ .cards #member-6 > .desc {
+#item-1:checked~.cards #member-1>.desc,
+#item-2:checked~.cards #member-2>.desc,
+#item-3:checked~.cards #member-3>.desc,
+#item-4:checked~.cards #member-4>.desc,
+#item-5:checked~.cards #member-5>.desc,
+#item-6:checked~.cards #member-6>.desc {
   display: block;
 }
 </style>
